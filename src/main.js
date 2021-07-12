992e68434dd2d4ea823411ff7d3248c4b1579966
xl <xl@qq.com>
2021-07-12 21:03:49 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false
 
-new Vue({
+Vue.use( ElementUi )
+
+new Vue( {
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h( App )
+} ).$mount( '#app' )
